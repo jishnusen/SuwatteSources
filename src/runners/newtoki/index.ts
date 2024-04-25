3a8e6971c66886dc1daf86fa2af1bef64db8594f
Jishnu Sen <jishnu1@gmail.com>
2024-04-25 15:46:31 -0700

    fix CF on homepage

--- src/runners/newtoki/index.ts
+++ src/runners/newtoki/index.ts
@@ -1,12 +1,23 @@
 import { SourceConfig, RunnerInfo } from "@suwatte/daisuke";
 import { TachiBuilder } from "../../template/tachiyomi";
+import { TachiHttpSource } from "../../template/tachiyomi/source";
 import { NewToki } from "./template";
 
 const INFO: RunnerInfo = {
   id: "newtoki",
   name: "NewToki",
   thumbnail: "newtoki.png",
-  version: 0.1,
+  version: 0.2,
+  website: "https://jishnusen.github.io/newtoki.html"
 };
 
-export const Target = new TachiBuilder(INFO, NewToki);
+class NewtokiBuilder extends TachiBuilder {
+  config?: SourceConfig = {
+    cloudflareResolutionURL: "https://jishnusen.github.io/newtoki.html",
+  }
+  constructor(info: RunnerInfo, template: new () => TachiHttpSource) {
+    super(info, template);
+  }
+}
+
+export const Target = new NewtokiBuilder(INFO, NewToki);
